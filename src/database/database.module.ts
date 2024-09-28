e0e14756cebbfe9297e9b15cb39582216a799393
Dyeison-Felipe <dyeisonfc@outlook.com>
2024-09-28 16:43:35 -0300

    created database module and service plus configutarion connection

---
+++ src/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { DatabaseConfigService } from './database.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
+    }),
+  ],
+  providers: [DatabaseConfigService],
+  exports: [DatabaseConfigService],
+})
+export class DatabaseModule {}
---
+++ src/database/database.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseConfigService {
+  constructor(private configService: ConfigService) {}
+
+  getDatabaseConfig() {
+    return {
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: this.configService.get<number>('DATABASE_PORT'),
+      user: this.configService.get<string>('DATABASE_USER'),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+    };
+  }
+}
